71fc01e63eecc20ab8aa88b704aa9e475e238124
RiccardoCarluccio <riccardo.carluccio.93@gmail.com>
2023-11-03 12:26:11 +0100

    create and set project

---
+++ src/App.vue
@@ -0,0 +1,18 @@
+<script>
+  export default {
+
+  }
+</script>
+
+<template>
+  test 123
+  <TheHeader></TheHeader>
+
+  <RouterView></RouterView>
+  
+  <TheFooter></TheFooter>
+</template>
+
+<style lang="scss">
+
+</style>
---
+++ src/router.js
@@ -0,0 +1,30 @@
+import { createRouter, createWebHistory } from "vue-router";
+
+import Home from "./pages/Home.vue";
+import Contacts from "./pages/Contacts.vue";
+import AboutUs from "./pages/AboutUs.vue";
+
+const routes = [                          //definizione rotte tramite array
+  {
+      path: "/",                          //uri da scrivere nel browser
+      name: "home",                       //nome della rotta da usare per creare un link
+      component: Home                     //componente che ritorna l'html della pagina
+  },
+  {
+      path: "/contacts",
+      name: "contacts",
+      component: Contacts
+  },
+  {
+      path: "/about-us",
+      name: "about-us",
+      component: AboutUs
+  },
+]
+
+const router = createRouter({				      //creazione istanza di Router
+  history: createWebHistory(),			      //gestione dell'url al cambio pagina
+  routes														      //l'array viene passato alle rotte
+});
+
+export { router };									      //esportazione istanza router per poterla usare dentro main.js
