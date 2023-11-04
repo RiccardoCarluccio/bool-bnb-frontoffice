66b25a06818c6ea6ad077ab2485368d5a2cddc57
Antonio9026 <121766004+Antonio9026@users.noreply.github.com>
2023-11-04 18:52:31 +0100

    terminato sezione contact e collegato a database

--- src/pages/Contacts.vue
+++ src/pages/Contacts.vue
@@ -1,4 +1,5 @@
 <script>
+import axios from "axios";
 export default {
   data() {
     return {
@@ -8,28 +9,44 @@ export default {
         message: ""
 
       },
+      errors: null,
+      succes: null
     };
+  },
+  methods: {
+    onFormSubmit() {
+      axios.post("http://127.0.0.1:8000/api/contacts", this.formData)
+      .then((resp) => {
+          this.succes = true;
+          this.errors = null;
+      })
+      .catch(e =>{
+         this.errors = e.message;
+      })
+    },
   },
 }
 </script>
 
 <template>
-  <div class="container">
-    <form @submit="onFormSubmit">
+  <div class="container" >
+    <div class="alert alert-danger" v-if="errors">Sembra che tu non abbia compilato tutti i campi: {{ errors }}</div>
+    <form @submit.prevent="onFormSubmit" v-if="!succes">
       <div class="mb-3">
         <label>Nome</label>
-        <input type="text" class="form-control" :model="formData.name">
+        <input type="text" class="form-control" v-model="formData.name">
       </div>
       <div class="mb-3">
         <label>Email</label>
-        <input type="text" class="form-control" :model="formData.email">
+        <input type="text" class="form-control" v-model="formData.email">
       </div>
       <div class="mb-3">
         <label>Messaggio</label>
-        <input type="text" class="form-control" :model="formData.message">
+        <textarea class="form-control" v-model="formData.message"></textarea>
       </div>
       <button type="submit">Invia</button>
     </form>
+    <div class="alert alert-success" v-else>Grazie per averci contattato. Vi ricontatteremo il prima possibile</div>
   </div>
 </template>
 
