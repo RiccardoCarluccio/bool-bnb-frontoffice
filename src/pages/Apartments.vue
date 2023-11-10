840371a3cbd9f7b15c6bf15108580c65b8b2b76b
masterkudi <cudini.andrea@gmail.com>
2023-11-10 18:45:40 +0100

    creazione card home + TheFooter

--- src/pages/Apartments.vue
+++ src/pages/Apartments.vue
@@ -23,25 +23,53 @@ export default {
 </script>
 
 <template>
-     
-  <div class="container" v-for="apartment in apartments" :key="apartment.id">
-    <div class="row">
-        <div class="col p-2 m-3 justify-content-between" style="border: .1px solid black">
-          <div class="card-image p-0 rounded-3">
-            <img src="" class="card-img-top rounded-0" alt="">
-          </div>
-          <div class="card-body h-50">
-            <div class="row d-flex">
-              <h2></h2>
-              <span class="text-decoration-none p-2 text-center">{{ apartment.name }}</span>
-              <span class="text-decoration-none p-2 text-center">{{ apartment.address }}</span>
-              <span class="text-decoration-none p-2 text-center">{{ apartment.description }}</span>
-              <span class="text-decoration-none p-2 text-center"></span>
-              <router-link :to="{ name:'apartments.show', params: { id: apartment.id }}">Singolo appartmaneto</router-link>
-            </div>
-          </div>
+  <div class="gallery">
+    <div class="row p-3 d-flex justify-content-center">
+      <div class="col-xxl-2 col-xl-3 col-lg-4 col-md-8 col-sm-12 m-2 p-2" v-for="apartment in apartments"
+        :key="apartment.id">
 
+        <div :id="'carouselExampleIndicators' + apartment.id" class="carousel slide">
+          <div class="carousel-indicators">
+            <button type="button" data-bs-target="#carouselExampleIndicators" data-bs-slide-to="0" class="active"
+              aria-current="true" aria-label="Slide 1"></button>
+            <button type="button" data-bs-target="#carouselExampleIndicators" data-bs-slide-to="1"
+              aria-label="Slide 2"></button>
+            <button type="button" data-bs-target="#carouselExampleIndicators" data-bs-slide-to="2"
+              aria-label="Slide 3"></button>
+          </div>
+          <div class="carousel-inner">
+            <div class="card-image p-0 rounded-3 carousel-item active"
+            :class="{ active: index === 0 }">
+              <img :src="apartment.images" class="card-img-top p-2 d-block w-100" style="height: 300px; object-fit: contain;"
+                alt="" />
+            </div>
+            <!-- <div v-for="(image, index) in apartment.images" :key="index" class="card-image p-0 rounded-3 carousel-item active"
+              :class="{ active: index === 0 }">
+              <img :src="apartment.images" class="card-img-top p-2 d-block w-100" style="height: 300px; object-fit: contain;"
+                alt="" />
+            </div> -->
+          </div> 
+          <button class="carousel-control-prev" type="button" data-bs-target="#carouselExampleIndicators"
+            data-bs-slide="prev">
+            <span class="carousel-control-prev-icon" aria-hidden="true"></span>
+            <span class="visually-hidden">Previous</span>
+          </button>
+          <button class="carousel-control-next" type="button" data-bs-target="#carouselExampleIndicators"
+            data-bs-slide="next">
+            <span class="carousel-control-next-icon" aria-hidden="true"></span>
+            <span class="visually-hidden">Next</span>
+          </button>
         </div>
+
+        <div class="card-body h-50">
+          <div class="row d-flex">
+            <h2></h2>
+            <span class="text-decoration-none"><router-link
+                :to="{ name: 'apartments.show', params: { id: apartment.id } }">{{ apartment.name }}</router-link></span>
+            <span class="text-decoration-none">{{ apartment.address }}</span>
+          </div>
+        </div>
+      </div>
     </div>
   </div>
 </template>
