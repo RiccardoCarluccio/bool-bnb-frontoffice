6602014baca2db8eb42d89b97b482abc2ee6a085
RiccardoCarluccio <riccardo.carluccio.93@gmail.com>
2023-11-13 17:35:40 +0100

    add const firstResult in axios call

--- src/components/ValleDiLacrime.vue
+++ src/components/ValleDiLacrime.vue
@@ -5,15 +5,22 @@
     data () {
       return {
         searchText: '',                       //indirizzo salvato dall'utente
-        results: [],                          //risultati ricerca
+        results: [],                          //risultati indirizzi                     DA CANCELLARE?
+        apartments: [],                       //risultati BnB
       }
     },
     methods: {
-      getAddress() {
+      getAddress() {                          //chiamata axios per trovare indirizzi, latitudine e longitudine
         axios.get(`https://api.tomtom.com/search/2/geocode/${this.searchText}.json?key=G3UqwADY39DYhuxHmuH49Pv68jOXjJTW`)
           .then((res) => {
-            this.results = res.data.results;
-          });
+            this.results = res.data.results;                                  //tutta la lista di indirizzi
+            const firstResult = res.data.results[0];                          //indirizzo più pertinente (primo della lista risultati)
+            if(firstResult) {
+              const latitude = firstResult.position.lat;                      //"latitudine" e "longitudine" vengono rese disponibili come variabili
+              const longitude = firstResult.position.lon;
+              this.getApartmentsWithinRadius(latitude, longitude);            //viene richiamata la funzione di calcolo distanza dal punto di ricerca
+            }
+         });
       },
     },
   }
@@ -21,14 +28,14 @@
 
 <template>
   <div class="container">
-    <input class="text-center" type="text" v-model="searchText" @keyup.enter="getAddress" placeholder="Inserisci un indirizzo">
+    <input class="text-center" type="text" v-model="searchText" @keyup="getAddress" placeholder="Inserisci un indirizzo">       <!-- la ricerca avviene alla pressione di ogni tasto -->
     <button @click="getAddress" type="submit" class="btn cerca_color mx-2">Cerca</button>
 
     <div v-if="results.length > 0">
-      <h2>Risultati della ricerca:</h2>
+      <h2>Lista indirizzi più pertinenti:</h2>
       <ul>
         <li v-for="(result, index) in results" :key="index">
-          {{ result.address.freeformAddress }}
+          {{ result.address.freeformAddress }}                        <!-- "freeformAddress" è una chiave dell'API TomTom -->
         </li>
       </ul>
     </div>
